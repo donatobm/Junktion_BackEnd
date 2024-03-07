4c47edb622e57528479094479d6be4fb1c824c61
donatobm <93447434+donatobm@users.noreply.github.com>
2024-03-07 16:23:03 -0400

    Chore: added new routes

--- src/routes/publications.routes.ts
+++ src/routes/publications.routes.ts
@@ -2,16 +2,26 @@ import express from "express";
 import {
     createPublication,
     getPublications,
+    likePublication,
+    editPublication,
+    getUserPublications,
+    getLikes,
+    getLikesPublication
 } from "../controllers/publications.controllers";
 import passport from "passport";
 
 const router = express.Router();
 
-router.get("/getPublications", passport.authenticate("jwt", { session: false }), getPublications);
+router.get("/get", passport.authenticate("jwt", { session: false }), getPublications);
 router.post(
-    "/createPublication",
+    "/create",
     passport.authenticate("jwt", { session: false }),
     createPublication
 );
+router.patch("/like/:publicationId", passport.authenticate("jwt", { session: false }), likePublication)
+router.put("/edit/:publicationId", passport.authenticate("jwt", { session: false }), editPublication)
+router.get("/get/:userId", passport.authenticate("jwt", { session: false }), getUserPublications)
+router.get("/like/:publicationId", passport.authenticate("jwt", { session: false }), getLikesPublication)
+router.get("/like", passport.authenticate("jwt", { session: false }), getLikes)
 
 export default router;
